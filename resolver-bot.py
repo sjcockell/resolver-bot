4ab31d9155f7dea1d1dcea50eae7d7203c408f80
Simon <sjcockell@gmail.com>
2009-10-23 22:29:23 +0100

    Robot is working, but doing nothing of note yet.
     Committer: Simon <sjcockell@gmail.com>
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   resolver-bot.py

--- resolver-bot.py
+++ resolver-bot.py
@@ -1,6 +1,8 @@
 from waveapi import events
 from waveapi import model
 from waveapi import robot
+import waveapi.document as doc
+import re
 
 def OnParticipantsChanged(properties, context):
 	"""Invoked when any participants have been added/removed."""
@@ -13,6 +15,17 @@ def OnRobotAdded(properties, context):
 	root_wavelet = context.GetRootWavelet()
 	root_wavelet.CreateBlip().GetDocument().SetText("I'm alive!")
 
+def OnBlipSubmit(properties, context):
+	"""Invoked whenever a blip is submitted"""
+	blip = context.GetBlipById(properties['blipId'])
+	contents = blip.GetDocument().GetText()
+	p = re.compile('\(up:P00001\)')
+	proteinlist = p.finditer(contents)
+	blip.CreateChild().GetDocument().SetText("You submitted a blip!")
+	for protein in proteinlist:
+		strip_contents = contents.replace(protein.group(0), 'P00001')
+		blip.GetDocument().SetText(strip_contents)
+
 def Notify(context):
 	root_wavelet = context.GetRootWavelet()
 	root_wavelet.CreateBlip().GetDocument().SetText("Hi everybody!")
@@ -20,8 +33,9 @@ def Notify(context):
 if __name__ == '__main__':
 	myRobot = robot.Robot('resolver-bot', 
 		image_url='http://resolver-bot.appspot.com/icon.png',
-		version='0.0.2',
+		version='0.0.3',
 		profile_url='http://resolver-bot.appspot.com/')
 	myRobot.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged)
 	myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
+	myRobot.RegisterHandler(events.BLIP_SUBMITTED, OnBlipSubmit)
 	myRobot.Run()
